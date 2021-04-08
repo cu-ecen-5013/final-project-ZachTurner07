c91a43272778ff41a839b60665a78a5bb7a7a2c6
Zach Turner <zachturner017@gmail.com>
2021-04-07 23:29:50 -0500

    setting fields of serial device

--- src/uartserver.c
+++ src/uartserver.c
@@ -6,6 +6,10 @@
 #include <termios.h> // Contains POSIX terminal control definitions
 #include <unistd.h> // write(), read(), close()
 
+
+// Resource
+// https://blog.mbedded.ninja/programming/operating-systems/linux/linux-serial-ports-using-c-cpp/#baud-rate
+
 // int main(int argc, char const *argv[])
 int main()
 {
@@ -14,6 +18,34 @@ int main()
     printf("Hello, world!\n");
 
     int serial_fid = open("/dev/ttyUSB0", O_RDWR);
+
+    if (serial_fid < 0)
+    {
+        perror("Couldn't open serial device");
+        exit(EXIT_FAILURE);
+    }
+
+    struct termios tty_device;
+
+    if(tcgetattr(serial_fid, &tty_device) != 0) 
+    {
+        perror("couldn't get tty attributes");
+        exit(EXIT_FAILURE);
+    }
+
+    tty_device.c_cflag &= ~PARENB; // no parity
+    tty_device.c_cflag &= ~CSTOPB; // 1 stop bit
+    tty_device.c_cflag &= ~CSIZE;
+    tty_device.c_cflag |= CS8 | CLOCAL;
+    tty_device.c_lflag = ICANON;
+    tty_device.c_oflag &= ~OPOST;
+
+    // set the baud rate to 9600 Kbaud
+    const speed_t baud_rate = B9600;
+    cfsetspeed(&tty_device, baud_rate);
+
+    tcsetattr(serial_fid, TCSANOW, &tty_device);
+    tcflush(serial_fid, TCOFLUSH);
 
     if (serial_fid < 0)
     {
