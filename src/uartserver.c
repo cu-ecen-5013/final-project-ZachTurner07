75179c2f13d5326dd53c87447486e905ebe82230
Zach Turner <zachturner017@gmail.com>
2021-04-07 23:11:48 -0500

    creating default read application

--- src/uartserver.c
+++ src/uartserver.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <fcntl.h> // Contains file controls like O_RDWR
+#include <errno.h> // Error integer and strerror() function
+#include <termios.h> // Contains POSIX terminal control definitions
+#include <unistd.h> // write(), read(), close()
 
 // int main(int argc, char const *argv[])
 int main()
@@ -6,6 +12,26 @@ int main()
     /* code */
 
     printf("Hello, world!\n");
+
+    int serial_fid = open("/dev/ttyUSB0", O_RDWR);
+
+    if (serial_fid < 0)
+    {
+        perror("opening serial device");
+        exit(EXIT_FAILURE);
+    }
+
+    char read_buffer [256];
+
+    int bytes_read = read(serial_fid, &read_buffer, sizeof(read_buffer));
+
+    if (bytes_read < 0)
+    {
+        perror("reading from serial port");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Bytes read from serial device: %s", read_buffer);
 
     return 0;
 }
